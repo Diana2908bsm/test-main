bba3a36f2af1bb949c4ddc456945a46330812d79
Diana2908bsm <92570731+Diana2908bsm@users.noreply.github.com>
2022-09-14 12:11:26 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(".components__column").slick({
+  dots: true,
+  arrows: false,
+  fade: true,
+  autoplay: true,
+});
+$(".help__column").slick({
+  dots: true,
+  arrows: false,
+  fade: true,
+  autoplay: true,
+});
